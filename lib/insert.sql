a0d2246b8a1669365b0cc3a4b6aa3bf27f134a63
Collins Chirchir <collinskchirchir@gmail.com>
2023-02-18 00:04:32 +0300

    done

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears(name, sex)  
+VALUES  ("Mr. Chocolate", "M"),
+        ("Rowdy", "M"),
+        ("Tabitha", "F"),
+        ("Sergeant Brown", "M"),
+        ("Melisssa", "F"),
+        ("Grinch", "M"),
+        ("Wendy", "F"),
+        (NULL, "M");
